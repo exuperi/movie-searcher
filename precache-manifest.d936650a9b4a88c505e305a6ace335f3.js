5c3e288a526feee223b35bbf488b494b018c791d
Siarhei Drozd <drozd1586229@gmail.com>
2020-03-03 21:25:50 +0100

    Updates

---
+++ precache-manifest.d936650a9b4a88c505e305a6ace335f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9f1d4324c6df7d3cec91084c84738c",
+    "url": "/movie-searcher/index.html"
+  },
+  {
+    "revision": "705de796335d95676014",
+    "url": "/movie-searcher/static/css/main.be2cc36e.chunk.css"
+  },
+  {
+    "revision": "ac0fa8a03e94b771a950",
+    "url": "/movie-searcher/static/js/2.25ad4212.chunk.js"
+  },
+  {
+    "revision": "705de796335d95676014",
+    "url": "/movie-searcher/static/js/main.567f168d.chunk.js"
+  },
+  {
+    "revision": "f23735500ca762c1228d",
+    "url": "/movie-searcher/static/js/runtime~main.ed79a067.js"
+  }
+]);
